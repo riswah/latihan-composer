bb8a7afd01ece92241090b0888b2a5b9bdba2e77
riswah <yudiriswah46@gmail.com>
2021-12-26 13:34:07 +0700

    first commit

---
+++ latihan library.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use MembuatLibrary\User;
+
+$ob = new User ();
+
+$ob->nama_user("yudi2");
---
+++ src/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace MembuatLibrary ;
+
+namespace MembuatLibrary;
+
+class User
+{
+    public function nama_user ($value)
+    {
+        echo $value;
+    }
+}
